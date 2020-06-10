0ea4eb880d21af37cc6266370b698191edd60818
Rishabh Srivas <srivash999@gmail.com>
2020-06-10 03:15:48 -0700

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,12 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
-    """"Test API View"""
+    """Test API View"""
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
@@ -58,10 +61,11 @@ class HelloViewSet(viewsets.ViewSet):
             name = serializer.validated_data.get('name')
             msg = f'Hello {name}'
             return Response({'message':msg})
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
     def retrieve(self, request, pk=None):
         """Handle getting an object by itd ID"""
@@ -78,3 +82,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
